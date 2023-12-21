9a26e83eca0563ceff332beb1df7d6275e67ebae
sharath kumar <you@example.com>
2023-12-21 08:44:07 +0530

    data types

---
+++ Mobile.java
@@ -0,0 +1,390 @@
+public class Mobile{
+public static void main(String[] args){
+
+byte BatteryPercentage=30;
+System.out.println("BatteryPercentage");
+BatteryPercentage=80;
+System.out.println("BatteryPercentage");	
+
+byte DaysinAmonth=30;
+System.out.println("DaysinAmonth");
+DaysinAmonth=31;
+System.out.println("DaysinAmonth");
+
+byte MonthsInYear=12;
+System.out.println(MonthsInYear);
+
+byte SpeedOfCar=45;
+System.out.println(SpeedOfCar);
+SpeedOfCar=60;
+System.out.println(SpeedOfCar);
+
+byte OnehourinMintutes=60;
+System.out.println(OnehourinMintutes);
+
+byte SachinHundreds=100;
+System.out.println(SachinHundreds);
+
+byte TalukinMandya=7;
+System.out.println(TalukinMandya);
+
+byte SemestersinEnginerring=6;
+System.out.println(SemestersinEnginerring);
+SemestersinEnginerring=8;
+System.out.println(SemestersinEnginerring);
+
+byte EnglishAlphabets=26;
+System.out.println(EnglishAlphabets);
+
+
+
+
+short studentsInXworkz=100;
+System.out.println(studentsInXworkz);
+
+studentsInXworkz=70;
+System.out.println(studentsInXworkz);
+
+short BooksinLibirary=6000;
+System.out.println(BooksinLibirary);
+BooksinLibirary=10000;
+System.out.println(BooksinLibirary);
+
+
+
+short employeesInACompany=500;
+System.out.println(employeesInACompany);
+
+employeesInACompany=1000;
+System.out.println(employeesInACompany);
+
+short priceOfMobile=20000;
+System.out.println(priceOfMobile);
+
+priceOfMobile=15000;
+System.out.println(priceOfMobile);
+
+short numberOfPeopleInTrain=3000;
+System.out.println(numberOfPeopleInTrain);
+
+numberOfPeopleInTrain=5000;
+System.out.println(numberOfPeopleInTrain);
+
+
+
+
+
+int salaryofSharath=50000;
+System.out.println(salaryofSharath);
+
+
+int MobileNumber=887878688;
+System.out.println(MobileNumber);
+MobileNumber=787767876;
+System.out.println(MobileNumber);
+
+int CarPrice=500000;
+System.out.println(CarPrice);
+CarPrice=7000006;
+System.out.println(CarPrice);
+
+int a=9787866;
+System.out.println(a);
+
+int number=4567890;
+System.out.println(number);
+
+int LaptopPrice=65000;
+System.out.println(LaptopPrice);
+
+int sachinRunsinInternationalRuns=756565;
+System.out.println(sachinRunsinInternationalRuns);
+
+int b=8897869;
+System.out.println(b);
+
+int c=576768;
+System.out.println(c);
+
+int d=456789;
+System.out.println(d);
+
+
+
+
+float Temperature=25.8f;
+System.out.println(Temperature);
+Temperature=28.8f;
+System.out.println(Temperature);
+
+float height=5.8f;
+System.out.println(height);
+height=5.7f;
+System.out.println(height);
+
+float valueofPie=3.142f;
+System.out.println(valueofPie);
+
+float ab=1.765f;
+System.out.println(ab);
+
+float bc=2.9786f;
+System.out.println(bc);
+
+float cd=3.76566f;
+System.out.println(cd);
+
+float da=25.886f;
+
+System.out.println(da);
+
+
+float max=6.775f;
+System.out.println(max);
+max=7.977f;
+System.out.println(max);
+
+
+long z=676577587l;
+System.out.println(z);
+
+z=564879l;
+System.out.println(z);
+
+long y=87766567l;
+System.out.println(y);
+
+y=123111l;
+System.out.println(y);
+
+long avg=45678l;
+System.out.println(avg);
+
+avg=345678l;
+System.out.println(avg);
+
+long sum=9875623l;
+System.out.println(sum);
+
+sum=8974562l;
+System.out.println(sum);
+
+long x=9875623l;
+System.out.println(x);
+
+x=8974562l;
+System.out.println(x);
+
+long w=458976l;
+System.out.println(w);
+
+long i=567886l;
+System.out.println(i);
+
+
+long n=43456l;
+System.out.println(n);
+
+long total=115493l;
+System.out.println(total);
+
+
+
+
+
+
+double height1=5.6786565d;
+System.out.println(height1);
+height1=5.566655676d;
+System.out.println(height1);
+
+double valueofPie2=3.1423455d;
+System.out.println(valueofPie2);
+
+double az=1.765576575464d;
+System.out.println(az);
+
+double bz=2.9786566d;
+System.out.println(bz);
+
+double cz=3.7656654d;
+System.out.println(cz);
+
+double dz=25.886d;
+System.out.println(dz);
+
+
+double max1=6.778686865d;
+System.out.println(max1);
+max1=7.9774554455d;
+System.out.println(max1);
+
+
+
+
+char nameIntial='k';
+System.out.println(nameIntial);
+
+nameIntial='s';
+System.out.println(nameIntial);
+
+char temp3='f';
+System.out.println(temp3);
+
+
+char min='d';
+System.out.println(max);
+
+min='d';
+System.out.println(max);
+
+char av='G';
+System.out.println(avg);
+
+av='K';
+System.out.println(avg);
+
+char add='Y';
+System.out.println(add);
+
+add='s';
+System.out.println(add);
+
+char pass='f';
+System.out.println(pass);
+
+pass='p';
+System.out.println(pass);
+
+char length='w';
+System.out.println(length);
+
+length='L';
+System.out.println(length);
+
+char weight='W';
+
+System.out.println(weight);
+
+
+char cmd='a';
+
+System.out.println(cmd);
+
+
+
+char max7='m';
+
+System.out.println(max7);
+
+
+
+
+boolean itsRaining=false;
+System.out.println(itsRaining);
+itsRaining=true;
+System.out.println(itsRaining);
+
+
+boolean itsJavaiseasy=true;
+System.out.println(itsJavaiseasy);
+itsJavaiseasy=false;
+System.out.println(itsJavaiseasy);
+
+
+boolean weekhasSevenDays=true;
+System.out.println(weekhasSevenDays);
+
+boolean itsBengaluruhasCaptialofKarnataka=true;
+System.out.println(itsBengaluruhasCaptialofKarnataka);
+
+boolean sundayisHoliday=true;
+System.out.println(sundayisHoliday);
+
+
+boolean itsengineeringhasFourYears=true;
+System.out.println(itsengineeringhasFourYears);
+
+boolean isViratKohliisCricketer=true;
+System.out.println(isViratKohliisCricketer);
+
+boolean myLaptopIsDell=true;
+System.out.println(myLaptopIsDell);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
