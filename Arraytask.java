a837459dacfb819f9c20b06e0454a94ae3b81439
sharath kumar <you@example.com>
2023-12-21 08:27:36 +0530

    array programing

---
+++ Arraytask.java
@@ -0,0 +1,55 @@
+public class Arraytask {
+
+public static void main(String[] args) {
+
+int []array={1,2,3,4,5,6,7,8,9,0,90,80,70,60,50,40,30,20,10,45};
+//System.out.println(array[2]);
+
+byte[]bytearray={(byte)1,(byte)2,(byte)3,(byte)4,(byte)5,(byte)6,(byte)7,(byte)8,(byte)9,(byte)10,(byte)11,(byte)12,(byte)13,(byte)14,(byte)15,(byte)16,(byte)17,(byte)18,(byte)19,(byte)20};
+//System.out.println(bytearray[4]);
+
+short[]shortarray={(short)20,(short)19,(short)18,(short)17,(short)16,(short)15,(short)14,(short)13,(short)12,(short)11,(short)10,(short)9,(short)8,(short)7,(short)6,(short)5,(short)4,(short)3,(short)2,(short)1};
+//System.out.println(shortarray[15]);
+
+long[]longarray={100,200,300,400,688,8687,6776,7878,6888,6879,4565,6867,6879,4556,9876,34566,4566,1223,3456,1234,3434};
+//System.out.println(longarray[10]);
+
+char[]chararray={'a','c','s','d','w','q','w','e','r','t','y','u','i','o','l','k','h','v','m','d'};
+//System.out.println(chararray[6]);
+
+float[]floatarray={2.45f,1.345f,1.766f,5.678f,6.89f,7.677f,5.777f,4.877f,8.988f,5.9887f,9.8766f,2.45f,1.345f,1.766f,5.678f,6.89f,7.677f,5.777f,4.877f,8.988f,5.9887f,9.8766f};
+//System.out.println(floatarray[7]);
+
+double[]doublearray={12.6777,33.4556,54.6778,45.887,34.877,12.6777,33.4556,54.6778,45.887,34.877,12.6777,33.4556,54.6778,45.887,34.877,12.6777,33.4556,54.6778,45.887,34.877,
+12.6777,33.4556,54.6778,45.887,34.877};
+//System.out.println(doublearray[8]);
+
+boolean[]booleanarray={true,false,true,true,false,true,false,true,true,false,true,false,true,true,false,true,false,true,true,false};
+//System.out.println(booleanarray[6]);
+
+for(int i=0;i<20; i++){
+System.out.println(array[i]);
+}
+for(int i=0;i<20; i++){
+System.out.println(bytearray[i]);
+}
+for(int i=0;i<20; i++){
+System.out.println(shortarray[i]);
+}
+for(int i=0;i<20; i++){
+System.out.println(longarray[i]);
+}
+for(int i=0;i<20; i++){
+System.out.println(chararray[i]);
+}
+for(int i=0;i<20; i++){
+System.out.println(floatarray[i]);
+}
+for(int i=0;i<20; i++){
+System.out.println(doublearray[i]);
+}
+for(int i=0;i<20; i++){
+System.out.println(booleanarray[i]);
+}
+}
+}
