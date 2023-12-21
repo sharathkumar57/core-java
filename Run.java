c12f78af431ed66009fe36d55f71c677a69aa6eb
sharath kumar <you@example.com>
2023-12-21 08:31:39 +0530

    looping

---
+++ Run.java
@@ -0,0 +1,284 @@
+public class Run{
+public static void main (String[] args){                      //looping
+	
+	int []intarray={1,2,3,4,5,6,7,8};
+	char []chararray={'a','b','c','d','e','f','g','h'};
+	short []shortarray={(short)1,(short)2,(short)3,(short)4,(short)5,(short)6,(short)7,(short)8};
+	int []sub={2312,4589,6778,7667,3467,8798,23987,4589};
+	int []div={1783,4856,7889,987,9967,8623,3945};
+	
+	for (int i=intarray.length-1;i>0; i--)         //in reverse
+	{
+		System.out.println(intarray[i]);
+	}
+	for (int i=chararray.length-1;i>0;i--)
+	{
+	System.out.println(chararray[i]);
+	}
+	for (int i=shortarray.length-1;i>0;i--)
+	{
+		System.out.println(shortarray[i]);
+	}
+	for (int i=sub.length-1;i>0;i--)
+	{
+		System.out.println(sub[i]);
+	}
+	for (int i=div.length-1;i>0;i--)
+	{
+		System.out.println(div[i]);
+	}
+	for (int i=(intarray.length-1)/2;i>-1; i--)         //in middle ltr
+	{
+		System.out.println(intarray[i]);
+	}
+	for (int i=(chararray.length-1)/2;i>-1;i--)
+	{
+		System.out.println(chararray[i]);
+	}
+	for (int i=(shortarray.length-1)/2;i>-1;i--)
+	{
+		System.out.println(shortarray[i]);
+	}
+	for (int i=(sub.length-1)/2;i>-1;i--)
+	{
+		System.out.println(sub[i]);
+	}
+	for (int i=(div.length-1)/2;i>-1;i--)
+	{
+		System.out.println(div[i]);
+	}
+	for (int i=(intarray.length-1)/2;i<intarray.length; i++)         //in middle rtl
+	{
+		System.out.println(intarray[i]);
+	}
+	for (int i=(chararray.length-1)/2;i<chararray.length;i++)
+	{
+		System.out.println(chararray[i]);
+	}
+	for (int i=(shortarray.length-1)/2;i<shortarray.length;i++)
+	{
+		System.out.println(shortarray[i]);
+	}
+	for (int i=(sub.length-1)/2;i<sub.length;i++)
+	{
+		System.out.println(sub[i]);
+	}
+	for (int i=(div.length-1)/2;i<div.length;i++)
+	{
+		System.out.println(div[i]);
+	}
+	for (int i=0;i<intarray.length; i++)      //even
+	{
+		if(intarray[i]%2==0)
+	
+	{
+		System.out.println(intarray[i]);
+	}
+	}
+	for (int i=0;i<chararray.length;i++)
+	{	
+	if(chararray[i]%2==0)
+	
+	{
+		System.out.println(chararray[i]);
+	}
+	}
+	for (int i=0;i<shortarray.length;i++)
+	{
+		if(shortarray[i]%2==0)
+	
+	{
+		System.out.println(shortarray[i]);
+	}
+	}
+	for (int i=0;i<sub.length;i++)
+	{
+		if(sub[i]%2==0)
+	{
+		System.out.println(sub[i]);
+	}
+	}
+	for (int i=0;i<div.length;i++)
+	{
+		if(div[i]%2==0)
+	
+	{
+		System.out.println(div[i]);
+	}
+	}
+	for (int i=0;i<intarray.length; i++)        //odd
+	{
+		if(intarray[i]%2!=0)
+	
+	{
+		System.out.println(intarray[i]);
+	}
+	}
+	for (int i=0;i<chararray.length;i++)
+	{	
+	if(chararray[i]%2!=0)
+	
+	{
+		System.out.println(chararray[i]);
+	}
+	}
+	for (int i=0;i<shortarray.length;i++)
+	{
+		if(shortarray[i]%2!=0)
+	
+	{
+		System.out.println(shortarray[i]);
+	}
+	}
+	for (int i=0;i<sub.length;i++)
+	{
+		if(sub[i]%2!=0)
+	{
+		System.out.println(sub[i]);
+	}
+	}
+	for (int i=0;i<div.length;i++)
+	{
+		if(div[i]%2!=0)
+	
+	{
+		System.out.println(div[i]);
+	}
+	}
+	int  sum=0;
+	for (int i=0;i<intarray.length; i++)        //sum
+	{
+		sum=sum+intarray[i];
+	
+	{
+		System.out.println(sum);
+	}
+	}
+	int num=0;
+	for (int i=0;i<chararray.length;i++)
+	{	
+	num=num+chararray[i];
+	
+	{
+		System.out.println(num);
+	}
+	}
+	int  add=0;
+	for (int i=0;i<shortarray.length;i++)
+	{
+		add=add+shortarray[i];
+	
+	{
+		System.out.println(add);
+	}
+	}
+	int sum1=0;
+	for (int i=0;i<sub.length;i++)
+	{
+		sum1=sum1+sub[i];
+	{
+		System.out.println(sum1);
+	}
+	}
+	int sum2=0;
+	for (int i=0;i<div.length;i++)
+	{
+	sum2=sum2+div[i];
+	
+	{
+		System.out.println(sum2);
+	}
+	}
+	
+
+
+	
+
+
+
+	
+	
+}
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+
+
+	
+
+
+	
+
+
+
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+
+
+	
+
+
+	
+
+
+
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+
